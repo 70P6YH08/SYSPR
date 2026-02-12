0dbffeb35dae616bed2459f2cc0ea1b3dda42b3a
RuttiTuttii <61704909+RuttiTuttii@users.noreply.github.com>
2026-02-12 12:02:03 +0300

    добавил отчёт по 6 лабе и 1-2 задания по ней

---
+++ Lecions/Disasm/Disasm/Disasm.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+int main()
+{
+    int x = 0;
+    const char* hello = "Hello Roman";
+
+    scanf("%d", &x);
+
+    int result = x * x;
+
+    printf("%d", result);
+}
