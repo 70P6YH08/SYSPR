1a5652bc7183699b42e83e08bc5246dab1c9376c
RuttiTuttii <61704909+RuttiTuttii@users.noreply.github.com>
2026-02-14 12:46:12 +0300

    доделал 8 лабу и добавил в отчёты

---
+++ Labs/Lab8/Lab8/FirstProject/FirstProject.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int num, sum = 0;
+    std::cout << "Введите последнее число: ";
+    std::cin >> num;
+
+    int counter = 1;
+    for (counter; counter <= num; counter++)
+    {
+        sum = sum + counter;
+    }
+    std::cout << sum;
+}
---
+++ Labs/Lab8/Lab8/SecondProject/SecondProject.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int counter, sum = 0;
+    std::cout << "Введите последнее число: ";
+    std::cin >> counter;
+
+    for (size_t i = 1; i <= counter; i++)
+    {
+        sum += i;
+    }
+    std::cout << sum;
+}
