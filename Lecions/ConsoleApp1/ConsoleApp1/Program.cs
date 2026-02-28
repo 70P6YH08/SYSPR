eb715328d4c39ab31ab144a99db8b91ec06f7ecb
RuttiTuttii <61704909+RuttiTuttii@users.noreply.github.com>
2026-02-28 10:10:34 +0300

    лекция по сборщику мусора, небезопасному коду (указателю) и терминалу

---
+++ Lecions/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿
+Person person = new Person() { name = "Roman", age = 23 };
+
+ChangePerson(ref person);
+
+Test();
+
+Console.WriteLine(person.name);
+Console.WriteLine(person.age);
+
+Console.WriteLine(GC.GetGeneration(person));
+Console.WriteLine(GC.GetTotalMemory(false));
+GC.Collect();
+GC.WaitForPendingFinalizers();
+Console.WriteLine(GC.GetTotalMemory(false));
+Console.WriteLine(GC.GetGeneration(person));
+
+GC.AddMemoryPressure(100000);
+GC.RemoveMemoryPressure(100000);
+
+unsafe
+{
+    int* x;
+    int y = 10;
+
+    x = &y;
+    Console.WriteLine(*x);
+    y = y + 10;
+    Console.WriteLine(*x);
+}
+
+void Test()
+{
+    using Person person1 = new Person() { name = "RIP" };
+
+
+    try
+    {
+        Person person2 = new Person();
+    }
+    finally
+    {
+        person.Dispose();
+    }
+}
+
+void ChangePerson(ref Person person)
+{
+    person.name = "NoName";
+    person = new Person() { name = "Bill", age = 123 };
+    Console.WriteLine(person.name);
+}
+
+class Person : IDisposable
+{
+    public string name = "";
+    public int age;
+
+    //~Person()
+    //{
+    //    Console.WriteLine($"{name} deleted");
+    //}
+
+    public void Dispose()
+    {
+        Console.WriteLine($"{name} deleted");
+        GC.SuppressFinalize(this);
+    }
+
+    //protected override void Finalize()
+    //{
+    //    try
+    //    {
+    //        //тут инструкция деструктора
+    //    }
+    //    finally
+    //    {
+    //        base.Finalize();
+    //    }
+    //}
+}
---
+++ Lecions/Terminal/Terminal/Program.cs
@@ -0,0 +1,14 @@
+﻿using Terminal.Gui;
+
+Application.Init();
+
+using Window window = new Window() { Title = "Hello World (Esc to quit)" };
+Label label = new()
+{
+    Text = "Hello, Terminal.Gui v2!",
+    X = Pos.Center(),
+    Y = Pos.Center()
+};
+window.Add(label);
+
+Application.Run(window);
