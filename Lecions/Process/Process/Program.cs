ec46668ebc73d04d16151e4fa404d663506193bc
RuttiTuttii <61704909+RuttiTuttii@users.noreply.github.com>
2026-03-10 16:13:49 +0300

    лекция по процессам

---
+++ Lecions/Process/Process/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+//Process.Start("notepad");
+
+//var startInfo = new ProcessStartInfo()
+//{
+//    FileName = "cmd.exe",
+//    UseShellExecute = false,
+//    RedirectStandardOutput = true,
+//    RedirectStandardError = true,
+//    RedirectStandardInput = true,
+//    CreateNoWindow = true,
+//};
+
+//using (var process = Process.Start(startInfo))
+//{
+//    using (var writer = process.StandardInput)
+//    {
+//        writer.WriteLine("ping ya.ru");
+//    }
+
+//    string output = process.StandardOutput.ReadToEnd();
+//    string error = process.StandardError.ReadToEnd();
+
+//    process.WaitForExit();
+
+//    Console.WriteLine($"Output: {output}");
+
+//    Console.WriteLine($"Error: {error}");
+//}
+
+var processes = Process.GetProcesses();
+
+foreach (var process in processes)
+{
+    Console.WriteLine($"Id: {process.Id}," +
+        $"Name: {process.ProcessName}" +
+        $"Handle: {process.VirtualMemorySize64 >> 30}");
+    //process.Kill();
+
+}
+
+var prc = Process.GetProcessesByName("notepad");
